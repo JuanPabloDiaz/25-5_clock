a3f9e20630dd8c6e8484dfdb2ef7fc44c2e72ba2
JuanPabloDiaz <juanchodis@hotmail.com>
2023-12-27 12:14:12 -0500

    Update layout responsive styling

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
+  // const isSmallScreen = window.innerWidth <= 640; // Adjust the breakpoint as needed
+
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#98C1D9] gap-2 sm:gap-3 md:gap-4 lg:gap-6">
+    <div className="flex flex-col pt-[30%] md:pt-[0%] justify-start md:justify-center items-center w-screen h-screen bg-[#98C1D9] gap-2 sm:gap-3 md:gap-4 lg:gap-6">
       {children}
     </div>
   );
