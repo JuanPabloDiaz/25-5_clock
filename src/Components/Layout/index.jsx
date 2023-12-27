381933504e0b64d47c8aa91eff393ee88b80b5c9
JuanPabloDiaz <juanchodis@hotmail.com>
2023-12-27 12:15:51 -0500

    Update layout component's top padding

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   // const isSmallScreen = window.innerWidth <= 640; // Adjust the breakpoint as needed
 
   return (
-    <div className="flex flex-col pt-[30%] md:pt-[0%] justify-start md:justify-center items-center w-screen h-screen bg-[#98C1D9] gap-2 sm:gap-3 md:gap-4 lg:gap-6">
+    <div className="flex flex-col pt-[20%] md:pt-[0%] justify-start md:justify-center items-center w-screen h-screen bg-[#98C1D9] gap-2 sm:gap-3 md:gap-4 lg:gap-6">
       {children}
     </div>
   );
